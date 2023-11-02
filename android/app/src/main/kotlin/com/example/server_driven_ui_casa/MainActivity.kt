6ae9c298b6ecbb06c774a091a29dcfb72407aef2
Fady Zaher <fady33zaher@gmail.com>
2023-11-02 12:12:55 +0200

    Server driven Ui

---
+++ android/app/src/main/kotlin/com/example/server_driven_ui_casa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.server_driven_ui_casa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:serve_dynamic_ui/serve_dynamic_ui.dart';
+import 'package:server_driven_ui_casa/custom_widgets/dy_widget_card.dart';
+
+void main() {
+  Map<String, DynamicWidgetHandler> widgetHandlerMap = {
+    "dy_widget_card": (json) => DynamicWidgetCard.fromJson(json)
+  };
+
+  ServeDynamicUI.init(
+    widgetHandlers: widgetHandlerMap,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomePage(title: 'Flutter Demo Home Page');
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ServeDynamicUIMaterialApp(
+      home: (context) {
+        return ServeDynamicUI.fromAssets('assets/json/sample.json');
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: false,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
